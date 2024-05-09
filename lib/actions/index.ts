ed6c978db9d8279b4eae77e660910e8c2c36469c
iritwiksingh <neetusingh02192gmail.com>
2024-05-09 10:40:09 +0530

    k

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,10 +1,10 @@
 "use server"
-
+import {getHighestPrice} from "../utils";
 import { revalidatePath } from "next/cache";
 import Product from "../models/product.model";
 import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
-import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
+import { getAveragePrice, getLowestPrice } from "../utils";
 import { User } from "@/types";
 import { generateEmailBody, sendEmail } from "../nodemailer";
 
