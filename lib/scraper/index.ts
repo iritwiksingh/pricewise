1bc0a8bc33c35ce7b3c26f38ce3c3f6f2fd6db8e
iritwiksingh <neetusingh02192gmail.com>
2024-05-09 09:43:05 +0530

    k

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -74,8 +74,8 @@ export async function scrapeAmazonProduct(url: string) {
       isOutOfStock: outOfStock,
       description,
       lowestPrice: Number(currentPrice) || Number(originalPrice),
-      highestPrice: Number(originalPrice) || Number(currentPrice),
-      averagePrice: Number(currentPrice) || Number(originalPrice),
+      highestPrice: Number(originalPrice) ,
+      averagePrice: (Number(currentPrice) +Number(originalPrice))/2,
     }
 
     return data;
